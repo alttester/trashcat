df7e5e44846f5b62379620fa710b23d39615d1a9
dorinoltean <dorinoltean@gmail.com>
2021-10-20 10:50:46 +0300

    updated to latest AUT  + added android build

--- TrashCat/Assets/Editor/BuildTrashCat.cs
+++ TrashCat/Assets/Editor/BuildTrashCat.cs
@@ -9,165 +9,181 @@ using Altom.AltUnity.Instrumentation;
 public class BuildTrashCat
 {
 
-    [MenuItem("Build/WindowsInspectorWithAltunity")]
-    static void WindowsBuildInspectorWithAltUnity()
-    {
-        WindowsBuildFromCommandLine(true, false, 13000);
-    }
-    [MenuItem("Build/WindowsBuildWithProxy")]
-    static void WindowsBuildWithProxy()
-    {
-        WindowsBuildFromCommandLine(true, true, 13000);
-    }
-    static void WindowsBuildFromCommandLine(bool withAltunity, bool withProxy, int port = 13000)
-    {
-        SetPlayerSettings();
+  [MenuItem("Build/WindowsWithAltunity")]
+  static void WindowsBuildInspectorWithAltUnity()
+  {
+    WindowsBuildFromCommandLine(true, 13000);
+  }
+  [MenuItem("Build/AndroidWithAltUnity")]
+  static void AndroidBuild()
+  {
+    AndroidBuildFromCommandLine(true, 13000);
+  }
 
-        Debug.Log("Starting Windows build..." + PlayerSettings.productName + " : " + PlayerSettings.bundleVersion);
-        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-        buildPlayerOptions.scenes = new string[]
-        {
+  [MenuItem("Build/macOSWithAltUnity")]
+  static void MacOSBuildInspectorWithAltUnity()
+  {
+    MacOSBuildFromCommandLine(true, 13000);
+  }
+  static void WindowsBuildFromCommandLine(bool withAltunity, int port = 13000)
+  {
+    SetPlayerSettings();
+
+    Debug.Log("Starting Windows build..." + PlayerSettings.productName + " : " + PlayerSettings.bundleVersion);
+    BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+    buildPlayerOptions.scenes = new string[]
+    {
             "Assets/Scenes/Main.unity",
             "Assets/Scenes/Shop.unity",
             "Assets/Scenes/Start.unity"
-        };
-        if (withAltunity)
-        {
-            buildPlayerOptions.locationPathName = "TrashCatWindowsTest/TrashCat.exe";
-
-        }
-        else
-        {
-            buildPlayerOptions.locationPathName = "TrashCatWindows/TrashCat.exe";
-
-        }
-        buildPlayerOptions.target = BuildTarget.StandaloneWindows;
-        buildPlayerOptions.targetGroup = BuildTargetGroup.Standalone;
-        if (withAltunity)
-        {
-            buildPlayerOptions.options = BuildOptions.Development;
-        }
-        BuildGame(buildPlayerOptions, withAltunity, withProxy, port);
+    };
+    if (withAltunity)
+    {
+      buildPlayerOptions.locationPathName = "TrashCatWindowsTest/TrashCat.exe";
 
     }
-
-    [MenuItem("Build/macOSWithAltUnity")]
-    static void MacOSBuildInspectorWithAltUnity()
+    else
     {
-        MacOSBuildFromCommandLine(true, false, 13000);
+      buildPlayerOptions.locationPathName = "TrashCatWindows/TrashCat.exe";
+
     }
-    private static void MacOSBuildFromCommandLine(bool withAltUnity, bool withProxy, int port = 13000)
+    buildPlayerOptions.target = BuildTarget.StandaloneWindows;
+    buildPlayerOptions.targetGroup = BuildTargetGroup.Standalone;
+    if (withAltunity)
     {
-        SetPlayerSettings();
-        PlayerSettings.macRetinaSupport = true;
+      buildPlayerOptions.options = BuildOptions.Development;
+    }
+    BuildGame(buildPlayerOptions, withAltunity, port);
 
-        Debug.Log("Starting Mac build..." + PlayerSettings.productName + " : " + PlayerSettings.bundleVersion);
-        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-        buildPlayerOptions.scenes = new string[]
-        {
+  }
+
+  static void AndroidBuildFromCommandLine(bool withAltunity, int port = 13000)
+  {
+    SetPlayerSettings();
+
+    Debug.Log("Starting Android build..." + PlayerSettings.productName + " : " + PlayerSettings.bundleVersion);
+    BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+    buildPlayerOptions.scenes = new string[]
+    {
             "Assets/Scenes/Main.unity",
             "Assets/Scenes/Shop.unity",
             "Assets/Scenes/Start.unity"
-        };
-        if (withAltUnity)
-        {
-            buildPlayerOptions.locationPathName = "TrashCatTest.app";
-
-        }
-        else
-        {
-            buildPlayerOptions.locationPathName = "TrashCat.app";
-
-        }
-        buildPlayerOptions.target = BuildTarget.StandaloneOSX;
-        buildPlayerOptions.targetGroup = BuildTargetGroup.Standalone;
-        if (withAltUnity)
-        {
-            buildPlayerOptions.options = BuildOptions.Development;
-        }
-
-        BuildGame(buildPlayerOptions, withAltUnity, withProxy, port);
-
-    }
-
-
-
-    private static void SetPlayerSettings()
+    };
+    if (withAltunity)
     {
-        PlayerSettings.companyName = "Altom";
-        PlayerSettings.productName = "TrashCat";
-        PlayerSettings.bundleVersion = "1.0";
-        PlayerSettings.resizableWindow = true;
-        PlayerSettings.defaultScreenHeight = 900;
-        PlayerSettings.defaultScreenWidth = 1200;
-        PlayerSettings.fullScreenMode = FullScreenMode.Windowed;
-        PlayerSettings.SetApiCompatibilityLevel(BuildTargetGroup.Standalone, ApiCompatibilityLevel.NET_4_6);
-        PlayerSettings.runInBackground = true;
-
+      buildPlayerOptions.locationPathName = "TraschatAndroidTest/TrashCat.apk";
     }
-
-
-
-
-    static void BuildGame(BuildPlayerOptions buildPlayerOptions, bool withAltUnity, bool withProxy, int port = 13000)
+    else
     {
-        try
-        {
-            if (withAltUnity)
-            {
-                AddAltUnity(buildPlayerOptions.targetGroup, buildPlayerOptions.scenes[0], withProxy, port);
-            }
-            var results = BuildPipeline.BuildPlayer(buildPlayerOptions);
+      buildPlayerOptions.locationPathName = "TraschatAndroid/TrashCat.apk";
 
-            if (withAltUnity)
-            {
-                RemoveAltUnity(buildPlayerOptions.targetGroup);
-            }
-
-            if (results.summary.totalErrors == 0)
-            {
-                Debug.Log("No Build Errors");
-
-            }
-            else
-            {
-                Debug.LogError("Build Error! " + results.steps + "\n Result: " + results.summary.result + "\n Stripping info: " + results.strippingInfo);
-                // EditorApplication.Exit(1);
-            }
-
-            Debug.Log("Finished. " + PlayerSettings.productName + " : " + PlayerSettings.bundleVersion);
-            // EditorApplication.Exit(0);
-        }
-        catch (Exception exception)
-        {
-
-            Debug.LogException(exception);
-            // EditorApplication.Exit(1);
-        }
     }
-    static void AddAltUnity(BuildTargetGroup buildTargetGroup, string firstSceneName, bool withProxy, int port = 13000)
+    buildPlayerOptions.target = BuildTarget.Android;
+    buildPlayerOptions.targetGroup = BuildTargetGroup.Android;
+    if (withAltunity)
     {
-        AltUnityBuilder.PreviousScenePath = firstSceneName;
-        var instrumentationSettings = new AltUnityInstrumentationSettings();
-        if (withProxy)
-        {
-            instrumentationSettings.InstrumentationMode = AltUnityInstrumentationMode.Proxy;
-            instrumentationSettings.ProxyPort = port;
-        }
-        else
-        {
-            instrumentationSettings.InstrumentationMode = AltUnityInstrumentationMode.Server;
-            instrumentationSettings.ServerPort = port;
-        }
-        AltUnityBuilder.AddAltUnityTesterInScritpingDefineSymbolsGroup(buildTargetGroup);
-        AltUnityBuilder.InsertAltUnityInScene(firstSceneName, instrumentationSettings);
-
+      buildPlayerOptions.options = BuildOptions.Development;
     }
-    static void RemoveAltUnity(BuildTargetGroup buildTargetGroup)
+    BuildGame(buildPlayerOptions, withAltunity, port);
+
+  }
+
+  private static void MacOSBuildFromCommandLine(bool withAltUnity, int port = 13000)
+  {
+    SetPlayerSettings();
+    PlayerSettings.macRetinaSupport = true;
+
+    Debug.Log("Starting Mac build..." + PlayerSettings.productName + " : " + PlayerSettings.bundleVersion);
+    BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+    buildPlayerOptions.scenes = new string[]
     {
-        AltUnityBuilder.RemoveAltUnityTesterFromScriptingDefineSymbols(buildTargetGroup);
+            "Assets/Scenes/Main.unity",
+            "Assets/Scenes/Shop.unity",
+            "Assets/Scenes/Start.unity"
+    };
+    if (withAltUnity)
+    {
+      buildPlayerOptions.locationPathName = "TrashCatTest.app";
+
+    }
+    else
+    {
+      buildPlayerOptions.locationPathName = "TrashCat.app";
+
+    }
+    buildPlayerOptions.target = BuildTarget.StandaloneOSX;
+    buildPlayerOptions.targetGroup = BuildTargetGroup.Standalone;
+    if (withAltUnity)
+    {
+      buildPlayerOptions.options = BuildOptions.Development;
     }
 
+    BuildGame(buildPlayerOptions, withAltUnity, port);
 
+  }
+
+
+  private static void SetPlayerSettings()
+  {
+    PlayerSettings.companyName = "Altom";
+    PlayerSettings.productName = "TrashCat";
+    PlayerSettings.bundleVersion = "1.0";
+    PlayerSettings.resizableWindow = true;
+    PlayerSettings.defaultScreenHeight = 900;
+    PlayerSettings.defaultScreenWidth = 1200;
+    PlayerSettings.fullScreenMode = FullScreenMode.Windowed;
+    PlayerSettings.SetApiCompatibilityLevel(BuildTargetGroup.Standalone, ApiCompatibilityLevel.NET_4_6);
+    PlayerSettings.runInBackground = true;
+
+  }
+  static void BuildGame(BuildPlayerOptions buildPlayerOptions, bool withAltUnity, int port = 13000)
+  {
+    try
+    {
+      if (withAltUnity)
+      {
+        AddAltUnity(buildPlayerOptions.targetGroup, buildPlayerOptions.scenes[0], port);
+      }
+      var results = BuildPipeline.BuildPlayer(buildPlayerOptions);
+
+      if (withAltUnity)
+      {
+        RemoveAltUnity(buildPlayerOptions.targetGroup);
+      }
+
+      if (results.summary.totalErrors == 0 || results.summary.result == UnityEditor.Build.Reporting.BuildResult.Succeeded)
+      {
+        Debug.Log("Build succeeded!");
+
+      }
+      else
+      {
+        Debug.LogError("Build failed! " + results.steps + "\n Result: " + results.summary.result + "\n Stripping info: " + results.strippingInfo);
+        // EditorApplication.Exit(1);
+      }
+
+      Debug.Log("Finished. " + PlayerSettings.productName + " : " + PlayerSettings.bundleVersion);
+      // EditorApplication.Exit(0);
+    }
+    catch (Exception exception)
+    {
+
+      Debug.LogException(exception);
+      // EditorApplication.Exit(1);
+    }
+  }
+  static void AddAltUnity(BuildTargetGroup buildTargetGroup, string firstSceneName, int port = 13000)
+  {
+    AltUnityBuilder.PreviousScenePath = firstSceneName;
+    var instrumentationSettings = new AltUnityInstrumentationSettings();
+    instrumentationSettings.ProxyPort = port;
+    AltUnityBuilder.AddAltUnityTesterInScritpingDefineSymbolsGroup(buildTargetGroup);
+    AltUnityBuilder.InsertAltUnityInScene(firstSceneName, instrumentationSettings);
+
+  }
+  static void RemoveAltUnity(BuildTargetGroup buildTargetGroup)
+  {
+    AltUnityBuilder.RemoveAltUnityTesterFromScriptingDefineSymbols(buildTargetGroup);
+  }
 
 }
